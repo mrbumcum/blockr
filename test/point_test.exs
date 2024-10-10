81002b6c27006dd54b7eb0e64370b52ef0e6262a
shucksow <102425362+shucksow@users.noreply.github.com>
2024-10-10 09:34:11 -0400

    test: Move down, left, right functions

---
+++ test/point_test.exs
@@ -0,0 +1,15 @@
+defmodule PointTest do
+  use ExUnit.Case
+
+  test "Move point down" do
+    assert Point.move_down({1, 1}) == {2, 1}
+  end
+
+  test "Move point right" do
+    assert Point.move_right({1, 1}) == {1, 2}
+  end
+
+  test "Move point left" do
+    assert Point.move_left({1, 1}) == {1, 0}
+  end
+end
